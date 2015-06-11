97072c96c4dfbf51ac356cd5906263f9b2b4f884
carl <zhongzheng.wang1986@gmail.com>
2015-06-11 08:43:34 +0800

    Summary: add if example
    Bug ID:
    Root Cause:
    Solution:
    ChangeLists:
    1. show how to use if statement in python
    2. attention to the return type of 'input'

--- 01_io.py
+++ 01_io.py
@@ -4,5 +4,9 @@
 name = input('please enter your name:')
 print('hello', name)
 
+# 注意, input返回的值是string类型, 即使你输入的是一个数字, 在条件判断语句里尤其要注意这点
+num = input('please enter a number:')
+print(type(num))
+
 # print可以打印多个参数, 参数用逗号','分隔, 遇到逗号',', python会
 # 输出一个空格
---
+++ 05_condition.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+age_str = input('please input your age:')
+
+# input返回的是string类型的值, 需要把string转换成数字类型, 才能
+# 用于下面的条件判断
+age_int = int(age_str)
+if age_int >= 18:
+    print('your age is', age_int)
+    print('adult')
+elif age_int >= 6:
+    print('your age is', age_int)
+    print('teenager')
+else:
+    print('your age is', age_int)
+    print('kid')
+
+
